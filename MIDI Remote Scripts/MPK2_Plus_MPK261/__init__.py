048588d6b469f45a5a3ed595d6f8320a52eb04c0
bangstk <bangstk@knights.ucf.edu>
2021-12-14 23:09:29 -0500

    Some init stuff in MPK261 was wrong

--- MIDI Remote Scripts/MPK2_Plus_MPK261/__init__.py
+++ MIDI Remote Scripts/MPK2_Plus_MPK261/__init__.py
@@ -8,7 +8,7 @@ from .MPK261_Plus import MPK261_Plus
 from _Framework.Capabilities import controller_id, inport, outport, CONTROLLER_ID_KEY, PORTS_KEY, NOTES_CC, SCRIPT, REMOTE
 
 def get_capabilities():
-	return {CONTROLLER_ID_KEY: controller_id(vendor_id=2536, product_ids=[36], model_name=u'MPK249 Plus'),
+	return {CONTROLLER_ID_KEY: controller_id(vendor_id=2536, product_ids=[37], model_name=u'MPK261 Plus'),
 	 PORTS_KEY: [inport(props=[NOTES_CC, SCRIPT, REMOTE]), outport(props=[SCRIPT, REMOTE])]}
 
 def create_instance(c_instance):
